f3aa211fe77860e0894cee03c6fb308f67cd6006
juanmaMTR <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
2021-10-20 11:35:49 +0200

    Commit 2

--- js/eventos.js
+++ js/eventos.js
@@ -2,16 +2,18 @@
 //Juan Manuel Toscano Reyes
 
 window.onload=iniciar
-
+let mapa=null
 
 function iniciar(){
-  let img=document.getElementsByTagName('img')[0]
+  mapa=document.getElementById('imgMapa')
   //img.onclick=manejadorClick
-  img.addEventListener('click',manejadorClick)
-  img.addEventListener('click',manejadorClick2)
+  //img.addEventListener('click',manejadorClick)
+  //img.addEventListener('click',manejadorClick2)
+  mapa.onclick=ponerSeta
+
 }
 
-
+/*
 function manejadorClick(){
   console.log('TRON');
 
@@ -19,4 +21,17 @@ function manejadorClick(){
 function manejadorClick2(){
   console.log('TRON2');
 
+}
+*/
+function ponerSeta(evento){
+  let seta=document.createElement('img')
+  seta.setAttribute('src','img/seta.png')
+  seta.classList.add('seta')
+  seta.style.top=`${evento.clientY -25}px`
+  seta.style.left=`${evento.clientX -25}px`
+  document.body.appendChild(seta)
+  seta.onContextMenu=eliminarSeta
+}
+function eliminarSeta(evento){
+  console.log(evento);
 }
